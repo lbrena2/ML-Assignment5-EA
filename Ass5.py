bb8443051e2a990ffc4167823709d155b937ca57
lbrena2 <l.brena2@campus.unimib.it>
2019-12-23 23:29:32 +0100

    Assignment report and code still bugged

--- Ass5.py
+++ Ass5.py
@@ -1,35 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.patches as mpatches
 
-#FIX-ME: ASK are this funct and the visualize_rastrigin implied in the rest of the ass or not?
+#FIX-ME: Add colour map, fix axes values
 #not general, only for R^2 -> R
-def visualize_sphere(x,y):
-    X,Y = np.meshgrid(x, y)
-    Z = (X**2 + Y**2)
-    #plt.contour(X, Y, Z)
-    #data = np.dstack((x,y))
-    #plt.scatter(x,y)
+def visualize_obj_function(objective_function):
+    coord = np.linspace(-5,5,100)
+    X,Y = np.meshgrid(coord,coord)
+    Z = objective_function(np.dstack([X,Y]))
+    plt.figure(1)
+    plt.clf()
+    contour = plt.contourf(X, Y, Z, 20, cmap='RdGy')
+    plt.colorbar()
+    # samples = np.random.uniform(-5,5,(100,2))
+    # plt.plot(samples[:, 0], samples[:, 1], 'ko')
     plt.show()
 
-#FIX-ME
-def visualize_rastrigin(x,y):
-    X,Y = np.meshgrid(x, y)
-    #Z = rastrigin_test(X,Y)
-    #plt.contour(X, Y, Z)
-    plt.show()
-
-#input of n dimension
 def sphere_test(data):
     z = np.sum(data**2, axis = -1)
     return z 
 
-#FIX-ME
 def rastrigin_test(data):
-    a = np.sum((data**2 - 10 * np.cos(2 * np.pi * data)), axis=1)
+    a = np.sum((data**2 - 10 * np.cos(2 * np.pi * data)), axis=-1)
     z = 10*data.shape[0] + a
+    # return (10 * dimension) + np.sum(np.square(samples) - A * np.cos(2 * np.pi * samples))
     return z
 
-# in this case the domain can be of arbitrary length
+def plot_samples_on_contour(samples, objective_function):
+    coord = np.linspace(-5,5,100)
+    X,Y = np.meshgrid(coord,coord)
+    Z = objective_function(np.dstack([X,Y]))
+    plt.figure(1)
+    plt.clf()
+    plt.contour(X, Y, Z)
+    plt.plot(samples[:, 0], samples[:, 1], 'ko')
+    plt.pause(0.1)
+
+
 def cem(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function):
     i = 0
     best_fitness_samples = np.zeros((number_of_generations, number_of_features))
@@ -37,27 +44,24 @@ def cem(mean, variance, number_of_generations, number_of_samples, number_of_feat
     while(i < number_of_generations):
         samples = np.random.normal(mean, variance, (number_of_samples, number_of_features))
         #evaluate using target function (the minus is for descending sorting)
-        #TIP: if something doesnt work as expected take a look to this sort
         idx = np.argsort(objective_function(samples))
         #select elite based on elite_set and refit gaussian
         mean = np.mean(samples[idx][:elite_set], axis=0)
         #TIP: to avoid variance vanishing add a costant
         variance = np.std(samples[idx][:elite_set], axis=0)
-        #variance += 0.1
+        #variance += 0.001
         #For plots
         best_fitness_samples[i] = samples[idx[0]]
         worst_fitness_samples[i] = samples[idx[-1]]
         #Plot in 2 dimensions
-        coord = np.linspace(-5,5,100)
-        X,Y = np.meshgrid(coord,coord)
-        Z = objective_function(np.dstack([X,Y]))
-        plt.figure(1)
-        plt.clf()
-        plt.contour(X, Y, Z)
-        plt.plot(samples[:, 0], samples[:, 1], 'ko')
-        plt.pause(1)
-
+        # plot_samples_on_contour(samples, objective_function)
         i += 1
+    # plt.plot(np.arange(0,number_of_generations), objective_function(best_fitness_samples), "r")
+    # plt.title('Number of Generations: ' + str(number_of_generations) + '\nPopulation number: ' + str(number_of_samples) + '\nNumber of features: ' + str(number_of_features)\
+    #     +'\n Elite set: '+ str(elite_set) + '\n Best fitness: ' +  str(objective_function(best_fitness_samples[-1])))
+    # plt.xlabel('Generation')
+    # plt.ylabel('Fitness')
+    # plt.show()
     return best_fitness_samples, worst_fitness_samples
 
 def cem_d(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function, number_of_runs):
@@ -67,10 +71,19 @@ def cem_d(mean, variance, number_of_generations, number_of_samples, number_of_fe
     worst_fitness =  np.zeros(number_of_generations)
     for _ in range(number_of_runs):
         best_fitness_samples, worst_fitness_samples = cem(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function)
-        best_fitness += sphere_test(best_fitness_samples)
-        worst_fitness += sphere_test(worst_fitness_samples)
+        best_fitness +=  objective_function(best_fitness_samples)
+        worst_fitness +=  objective_function(worst_fitness_samples)
+    #prints for the report:
+    # print("Avg best fitness:" + str(np.min(best_fitness/number_of_runs)))
+    # print("Avg worst fitness:" + str(np.min(worst_fitness/number_of_runs)))
     #CHECK: is the order of the operation (sum, mean, sphere_test) correct?
-    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", np.arange(0,number_of_generations), worst_fitness/number_of_runs)
+    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", label="best fitness")
+    plt.plot(np.arange(0,number_of_generations), worst_fitness/number_of_runs, "b", label="worse fitness")
+    plt.title('Number of Generations: ' + str(number_of_generations) + '\nPopulation number: ' + str(number_of_samples) + '\nNumber of features: ' + str(number_of_features)\
+        +'\n Elite set: '+ str(elite_set))
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.legend(loc="upper right")
     plt.show()
 
 def nes(mean, variance, number_of_generations, number_of_samples, number_of_features, objective_function, learning_rate):
@@ -83,49 +96,28 @@ def nes(mean, variance, number_of_generations, number_of_samples, number_of_feat
     while(i < number_of_generations):
         samples = np.random.normal(mean, variance, (number_of_samples, number_of_features))
         evaluated_samples = objective_function(samples)
-        #Maybe this for is useless
-        # for j in range(number_of_samples):
-            # log_derivatives_mean[j] = (samples[j] - mean) / variance**2
-            # log_derivatives_variance[j] = ((samples[j] - mean)**2 - variance**2) / variance**3
+        
         log_derivatives_mean = (samples - mean) / variance**2
         log_derivatives_variance = ((samples - mean)**2 - variance**2) / variance**3
-
         log_derivatives_mean_f = evaluated_samples[..., np.newaxis] * log_derivatives_mean
         log_derivatives_variance_f = evaluated_samples[..., np.newaxis] * log_derivatives_variance
 
         j_derivatives_mean = np.sum(log_derivatives_mean_f, axis=0) / number_of_samples
         j_derivatives_variance = np.sum(log_derivatives_variance_f, axis=0) / number_of_samples
 
-        #outer_prod_mean = np.zeros((number_of_features, number_of_features, number_of_samples))
-        #for k,sample in enumerate(log_derivatives_mean):
-        #    outer_prod_mean[:,:,k] = np.outer(sample,sample)
-        #F_mean = np.sum(outer_prod_mean, axis=-1) / number_of_samples
         F_mean = np.matmul(np.transpose(log_derivatives_mean), log_derivatives_mean) / number_of_samples
-        
-        # outer_prod_variance = np.zeros((number_of_features, number_of_features, number_of_samples))
-        # for k,sample in enumerate(log_derivatives_variance):
-        #     outer_prod_variance[:,:,k] = np.outer(sample,sample)
-        # F_variance = np.sum(outer_prod_variance, axis=-1) / number_of_samples
         F_variance = np.matmul(np.transpose(log_derivatives_variance), log_derivatives_variance) / number_of_samples
-        
-        
+
         mean = mean - learning_rate * (np.matmul(np.linalg.inv(F_mean),j_derivatives_mean))
         variance = variance - learning_rate * (np.matmul(np.linalg.inv(F_variance),j_derivatives_variance))
+        variance += 0.01
 
         #For plots
         idx = np.argsort(evaluated_samples)
         best_fitness_samples[i] = samples[idx[0]]
         worst_fitness_samples[i] = samples[idx[-1]]
-         #Plot in 2 dimensions
-        # if i % 50 == 0:
-        #     coord = np.linspace(-5,5,100)
-        #     X,Y = np.meshgrid(coord,coord)
-        #     Z = objective_function(np.dstack([X,Y]))
-        #     plt.figure(1)
-        #     plt.clf()
-        #     plt.contour(X, Y, Z)
-        #     plt.plot(samples[:, 0], samples[:, 1], 'ko')
-        #     plt.pause(0.1)
+        #Plot in 2 dimensions
+        # plot_samples_on_contour(samples, objective_function)
         i += 1
     return best_fitness_samples, worst_fitness_samples
 
@@ -138,8 +130,17 @@ def nes_d(mean, variance, number_of_generations, number_of_samples, number_of_fe
         best_fitness_samples, worst_fitness_samples = nes(mean, variance, number_of_generations, number_of_samples, number_of_features, objective_function, learning_rate)
         best_fitness += sphere_test(best_fitness_samples)
         worst_fitness += sphere_test(worst_fitness_samples)
+    #prints for the report:
+    print("Avg best fitness:" + str(np.min(best_fitness/number_of_runs)))
+    print("Avg worst fitness:" + str(np.min(worst_fitness/number_of_runs)))
     #CHECK: is the order of the operation (sum, mean, sphere_test) correct?
-    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", np.arange(0,number_of_generations), worst_fitness/number_of_runs)
+    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", label="best fitness")
+    plt.plot(np.arange(0,number_of_generations), worst_fitness/number_of_runs, "b", label="worse fitness")
+    plt.title('Number of Generations: ' + str(number_of_generations) + '\nPopulation number: ' + str(number_of_samples) + '\nNumber of features: ' + str(number_of_features)\
+        +'\n Elite set: '+ str(elite_set))
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.legend(loc="upper right")
     plt.show()
 
 def cmaes(mean, covariance_matrix, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function):
@@ -147,33 +148,23 @@ def cmaes(mean, covariance_matrix, number_of_generations, number_of_samples, num
     best_fitness_samples = np.zeros((number_of_generations, number_of_features))
     worst_fitness_samples =  np.zeros((number_of_generations, number_of_features))
     while(i < number_of_generations):
-        #CHECK: just one dimension, is it fine?
-        #CHECK: after the first iteration the covariance is no more positive-semidefinite
         samples = np.random.multivariate_normal(mean, covariance_matrix, (number_of_samples))
         # Remember without minus is minimizing
         idx = np.argsort(objective_function(samples))
         # Update covariance first avoids this backup
-        old_mean = mean
-        mean = np.mean(samples[idx][:elite_set], axis=0)
         covariance_matrix = np.zeros((number_of_features, number_of_features))
         for ii in range(number_of_features):
             for j in range(number_of_features):
                 for sample in samples[idx][:elite_set]:
-                    covariance_matrix[ii,j] += (sample[ii] - old_mean[ii])*(sample[j] - old_mean[j])
+                    covariance_matrix[ii,j] += (sample[ii] - mean[ii])*(sample[j] - mean[j])
         covariance_matrix = covariance_matrix / elite_set
+        mean = np.mean(samples[idx][:elite_set], axis=0)
         #For plots
         best_fitness_samples[i] = samples[idx[0]]
         worst_fitness_samples[i] = samples[idx[-1]]
 
         #Plot in 2 dimensions
-        coord = np.linspace(-5,5,100)
-        X,Y = np.meshgrid(coord,coord)
-        Z = objective_function(np.dstack([X,Y]))
-        plt.figure(1)
-        plt.clf()
-        plt.contour(X, Y, Z)
-        plt.plot(samples[:, 0], samples[:, 1], 'ko')
-        plt.pause(0.1)
+        # plot_samples_on_contour(samples, objective_function)
         
         i += 1
     return best_fitness_samples, worst_fitness_samples
@@ -187,9 +178,19 @@ def cmaes_d(mean, variance, number_of_generations, number_of_samples, number_of_
         best_fitness_samples, worst_fitness_samples = cmaes(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function)
         best_fitness += objective_function(best_fitness_samples)
         worst_fitness += objective_function(worst_fitness_samples)
+    #prints for the report:
+    print("Avg best fitness:" + str(np.min(best_fitness/number_of_runs)))
+    print("Avg worst fitness:" + str(np.min(worst_fitness/number_of_runs)))
     #CHECK: is the order of the operation (sum, mean, sphere_test) correct?
-    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", np.arange(0,number_of_generations), worst_fitness/number_of_runs)
+    plt.plot(np.arange(0,number_of_generations), best_fitness / number_of_runs, "r", label="best fitness")
+    plt.plot(np.arange(0,number_of_generations), worst_fitness/number_of_runs, "b", label="worse fitness")
+    plt.title('Number of Generations: ' + str(number_of_generations) + '\nPopulation number: ' + str(number_of_samples) + '\nNumber of features: ' + str(number_of_features)\
+        +'\n Elite set: '+ str(elite_set))
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness')
+    plt.legend(loc="upper right")
     plt.show()
+
         
         
 
@@ -199,21 +200,21 @@ def cmaes_d(mean, variance, number_of_generations, number_of_samples, number_of_
 
 
 ### Sphere test plot, change with np.random.normal()
-x = np.linspace(-5,5,100)
-y = np.linspace(-5,5,100)
+# x = np.linspace(-5,5,100)
+# y = np.linspace(-5,5,100)
 #visualize_sphere(x,y)
 #g = np.linspace(-5,5,100)
 
 ### cem test 
-number_of_generations = 500
-number_of_samples = 10000
-number_of_features = 100
-elite_set = 2000
+number_of_generations = 50
+number_of_samples = 100
+number_of_features = 20
+elite_set = 20
 objective_function = sphere_test
 number_of_runs = 3
 mean = np.random.uniform(-5,5,number_of_features)
 variance = np.random.uniform(0,5,number_of_features)
-learning_rate = 0.01
+learning_rate = 0.001
 
 #covariance matrix. CHECK: is correct to use normal?
 covariance_matrix = np.diag(np.random.uniform(-5,5,number_of_features))
@@ -221,15 +222,20 @@ covariance_matrix = np.diag(np.random.uniform(-5,5,number_of_features))
 
 # cem(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function)
 # cem_d(mean, variance, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function, number_of_runs)
-nes_d(mean, variance, number_of_generations, number_of_samples, number_of_features, objective_function, learning_rate, number_of_runs)
-#cmaes_d(mean, covariance_matrix, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function, number_of_runs)
+# nes_d(mean, variance, number_of_generations, number_of_samples, number_of_features, objective_function, learning_rate, number_of_runs)
+cmaes_d(mean, covariance_matrix, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function, number_of_runs)
 #cmaes(mean, covariance_matrix, number_of_generations, number_of_samples, number_of_features, elite_set, objective_function)
 # nes(mean, variance, number_of_generations, number_of_samples, number_of_features, objective_function, learning_rate)
+# visualize_obj_function(objective_function)
 
+# plt.show()
 
-plt.show()
-
-## It's not cheating:
-# - Use tensor flow, pythorch for the gradient calc, either use guthub but i have to cite the source 
+## Exercise 1
+# visualize_obj_function(objective_function)
+# samples = np.random.uniform(-5,5,(100,2))
+# plt.scatter(samples[:, 0], samples[:, 1], c= objective_function(samples), s=500, cmap='RdGy')
+# plt.colorbar()
+# plt.show()
+##
 
 
